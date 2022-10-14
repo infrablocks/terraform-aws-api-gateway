fcb6aafaf614f3aa3d0831366031b4d0a8aa3262
Liam Griffin-Jowett <gryffskin@gmail.com>
2022-10-14 13:52:12 +0100

    move certificate spec to rspec terraform style tests

--- spec/unit/certificate_spec.rb
+++ spec/unit/certificate_spec.rb
@@ -1,17 +1,31 @@
-# # frozen_string_literal: true
-#
-# require 'spec_helper'
-#
-# describe 'Certificate' do
-#   subject { acm("#{subdomain}.#{domain_name}") }
-#
-#   include_context 'terraform'
-#
-#   let(:component) { vars.component }
-#   let(:name) { output_for(:harness, 'name') }
-#   let(:deployment_identifier) { vars.deployment_identifier }
-#   let(:subdomain) { vars.subdomain }
-#   let(:domain_name) { vars.domain_name }
-#
-#   # it {should exist}
-# end
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe 'Certificate' do
+  before(:context) do
+    @plan = plan(role: :root) do |vars|
+      vars.create_certificate = 'yes'
+      vars.create_custom_domain = 'yes'
+    end
+  end
+
+  let(:component) do
+    var(role: :root, name: 'component')
+  end
+  let(:deployment_identifier) do
+    var(role: :root, name: 'deployment_identifier')
+  end
+
+  let(:name) do
+    output(role: :root, name: 'name')
+  end
+  let(:subdomain) do
+    output(role: :root, name: 'subdomain')
+  end
+  let(:domain_name) do
+    output(role: :root, name: 'domain_name')
+  end
+
+  it { is_expected.to exist }
+end
