24cc3b1354e1b22e06a480dc35220f0bff96bd5a
Toby <tobyclemson@gmail.com>
2022-11-23 22:26:54 -0300

    Always use lowercase seeds.

--- spec/unit/infra/prerequisites/main.tf
+++ spec/unit/infra/prerequisites/main.tf
@@ -11,7 +11,7 @@ module "certificate" {
   source  = "infrablocks/acm-certificate/aws"
   version = "1.2.0-rc.1"
 
-  domain_name = lower(var.domain_name)
+  domain_name = var.domain_name
   domain_zone_id = var.public_zone_id
 
   subject_alternative_names = []
