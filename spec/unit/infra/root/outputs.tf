5127c59b2c4e22cc5853f05ae6ff14f06617d14a
Toby <tobyclemson@gmail.com>
2022-12-02 10:12:32 -0300

    Expose output in test harness.

--- spec/unit/infra/root/outputs.tf
+++ spec/unit/infra/root/outputs.tf
@@ -9,3 +9,7 @@ output "api_gateway_rest_api_id" {
 output "api_gateway_rest_api_root_resource_id" {
   value = module.api_gateway.api_gateway_rest_api_root_resource_id
 }
+
+output "api_gateway_redeployment_triggers" {
+  value = api_gateway_redeployment_triggers
+}
